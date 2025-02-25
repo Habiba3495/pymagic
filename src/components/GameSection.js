10e7fae05486b2307379d377f2ab914291bf0985
shahd <shahdkhalid204@gmail.com>
2025-02-26 01:58:23 +0200

    done

--- src/components/GameSection.js
+++ src/components/GameSection.js
@@ -3,13 +3,19 @@ import Lsidebar from "./Lsidebar"; // استيراد الشريط الجانبي
 import "./GameSection.css";
 
 const GameSection = () => {
-
+  const startGame = () => {
+    console.log("Game started!"); 
+  };
 
   return (
     <div className="Gpagecontainer">
-      <Lsidebar />  {/* الشريط الجانبي */}
-      <div className="G">
-      < h2>game</h2>
+      <Lsidebar /> {/* الشريط الجانبي */}
+      <div className="game-background">
+        <div className="game-door">
+          <button className="start-game-btn" onClick={startGame}>
+            Start Game
+          </button>
+        </div>
       </div>
     </div>
   );
